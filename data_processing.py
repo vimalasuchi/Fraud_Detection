980819db0d70cacf9308ac5f393bd5919f97b90c
vimalasuchi <vimalasuchi@yahoo.co.uk>
2024-04-01 08:25:52 +0530

    Trying hard

---
+++ data_processing.py
@@ -0,0 +1,19 @@
+print ("addimg all code to data processing")
+a=5
+b=10
+def sum(a,b):
+    c=a+b
+    return c
+
+print("Addition is now c ", c)
+
+def multiplication(a,b):
+    d=a*b
+    return d
+
+print("multiplication of ", d)
+
+def div(a,b):
+    e=a/b
+    return e
+print("Division of a & b", e)
